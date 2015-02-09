51fc87fc04a8426c1e70a932786001b774d09492
jll1991 <qhdtk1991@naver.com>
2015-02-09 14:41:55 +0900

    UserServiceImpl 수정 application-config에 transaction 추가 mybatis_config에 mapper 추가

--- EasyDel/src/main/java/easydel/service/UserServiceImpl.java
+++ EasyDel/src/main/java/easydel/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import easydel.dao.IUserDao;
 import easydel.entity.User;
@@ -21,40 +22,29 @@ public class UserServiceImpl implements IUserService {
 	private SqlSession session;
 	
 	@Override
+	@Transactional
 	public void serviceRegistrateNewUser(User user) throws DuplicatedIdException, ServiceFailException {
 		this.serviceCheckDuplicatedId(user.getUserId());
 		
 		int result = 0;
-		
 		result = dao.insertUser(user);
 		
 		if(result <= 0) {
 			throw new ServiceFailException();
 		}
 	}
+	
 	@Override
+	@Transactional
 	public void serviceCheckDuplicatedId(String userId) throws DuplicatedIdException, ServiceFailException {
-//		if(userId == null) {
-//			throw new ServiceFailException();
-//		}
-//		
-//		User result = null;
-//		
-//		try {
-//			result = userImpl.selectUserById(session, userId);
-//		} catch (RuntimeException e) {
-//			LOG.error("selectUserById를 DB에서 실행중 에러 발생", e);
-//			session.close();
-//			throw new ServiceFailException();
-//		} finally {
-//			session.close();
-//		}
-//		if(result != null) {
-//			throw new DuplicatedIdException();
-//		}
+		User user = dao.selectUserByUserId(userId);
+		if(user != null) {
+			throw new DuplicatedIdException();
+		}
 	}
 	
 	@Override
+	@Transactional
 	public boolean LoginService(String userId, String userPassword) {
 		boolean flag = false;
 		User user;
