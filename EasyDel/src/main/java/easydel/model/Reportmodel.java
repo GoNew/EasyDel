5cc4f0b9c896a856472771a6e0163baa8370a6c2
ce401-08 <dfdf@naver.com>
2015-02-11 11:52:13 +0900

    태성 점심커밋

---
+++ EasyDel/src/main/java/easydel/model/Reportmodel.java
@@ -0,0 +1,19 @@
+package easydel.model;
+
+public class Reportmodel {
+
+	private String reportdesc;
+
+	public Reportmodel(String reportdesc) {
+		super();
+		this.reportdesc = reportdesc;
+	}
+
+	public String getReportdesc() {
+		return reportdesc;
+	}
+
+	public void setReportdesc(String reportdesc) {
+		this.reportdesc = reportdesc;
+	}
+}
